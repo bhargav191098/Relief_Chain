5e501a588c319d16b9807e244850664551a5ebeb
Bhargav Krish <bhargav3514@gmail.com>
2018-09-01 18:54:34 +0530

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,78 @@
+from flask import Flask
+
+from flask import jsonify
+  
+import MySQLdb
+ 
+import gc
+
+import json
+   
+app = Flask(__name__)
+   
+app.debug = True
+   
+# database connection method
+ 
+def connection():
+ 
+	conn = MySQLdb.connect(
+	 
+	host = "localhost",
+	 
+	user = "root",
+	 
+	passwd = "1234567890",
+	 
+	db = "users"
+	 
+	)
+	 
+ 
+ 
+	c = conn.cursor()
+	 
+	return c, conn
+	   
+c, con = connection()
+@app.route("/")
+ 
+def hello():
+ 
+    return 'WUBBA LUBBA DUB DUB'
+   
+ 
+@app.route("/get/")
+ 
+def get_data():
+   
+	c.execute("SELECT * FROM login")
+	 
+	users = c.fetchall()
+	 
+
+	 
+	 
+	data = '{'
+	 
+	for user in users:
+	
+	 
+		data = data + '"'+user[0]+'"' + ': ' +' "'+user[2]+'"' + ','
+	 
+
+	data = data+'"'+'12'+'"'+':'+'"'+'21'+'"'+'}'
+	
+
+		
+	 
+	gc.collect()
+	 
+
+	 
+	 
+	return jsonify(data)
+
+if __name__ == "__main__":
+ 
+	app.run(host='0.0.0.0',debug='True')
